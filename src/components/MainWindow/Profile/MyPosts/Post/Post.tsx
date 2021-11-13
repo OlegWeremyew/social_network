71d507cb1ae2d4263dc71a2e35253b8c0efd90d0
olegweremyew <olegweremey1994@mail.ru>
2021-11-13 23:28:04 +0300

    CSS in Post

--- src/components/MainWindow/Profile/MyPosts/Post/Post.tsx
+++ src/components/MainWindow/Profile/MyPosts/Post/Post.tsx
@@ -12,7 +12,7 @@ const Post = (props: PostMessageType) => {
             <img
                 src="https://slovami.net/wp-content/uploads/thumbs_dir/the-most-beautiful-guys-russia-4-1-nwkkkydf2f1mhw9a4ijzfs2ktasvevyjiyi4xibqis.jpg"
                 alt="ava" title="ava"/>
-            <div>
+            <div className={c.itemText}>
                 {props.message}
                 <div>
                     <span>like: {props.likesCount}</span>
