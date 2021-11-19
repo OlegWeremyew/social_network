35331444b699643a42ac3c68ff7eaca65cd9aa5b
OlegWeremyew <olegweremey1994@mail.ru>
2021-11-19 11:04:56 +0300

    added data in Index

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,17 @@ import React from 'react';
 import c from './App.module.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/MainWindow/Profile/Profile";
 import Footer from "./components/Footer/Footer";
-import Messages, {MessageType, UsersType} from "./components/MainWindow/Messages/Messages";
 import {Route, Routes} from "react-router-dom";
 import News from "./components/MainWindow/News/News";
 import Music from "./components/MainWindow/Music/Music";
 import Friends from "./components/MainWindow/Friends/Friends";
 import Settings from "./components/MainWindow/Settings/Settings";
-import {PostMessageType} from "./components/MainWindow/Profile/MyPosts/Post/Post";
+import {AppType} from "./index";
+import Messages from "./components/MainWindow/Messages/Messages";
+import Profile from "./components/MainWindow/Profile/Profile";
 
-function App() {
-
-    let posts: Array<PostMessageType> = [
-        {message: "Hello", likesCount: 12, id: 1},
-        {message: "Dinosaurus are great", likesCount: 17, id: 2}
-    ]
-
-    let users: Array<UsersType> = [
-        {name: "Oleg", id: 1},
-        {name: "Diana", id: 2},
-        {name: "Fat cat", id: 2},
-        {name: "Dimka", id: 4},
-        {name: "Homka", id: 5},
-        {name: "Belka", id: 6},
-    ]
-    let messages: Array<MessageType> = [
-        {message: "Hi", id: 1},
-        {message: "Ho", id: 2},
-        {message: "He", id: 3},
-        {message: "Hu", id: 4},
-        {message: "Hio", id: 5},
-        {message: "Hia", id: 6},
-    ]
+function App(props: AppType) {
 
     return (
         <main className={c.appContent}>
@@ -42,8 +20,8 @@ function App() {
             <Navbar/>
             <div className={c.appContentWindow}>
                 <Routes>
-                    <Route path="/profile/*" element={<Profile posts={posts} />}/>
-                    <Route path="/messages/*" element={<Messages users={users} messages={messages}/>}/>
+                    <Route path="/profile/*" element={<Profile posts={props.posts}/>}/>
+                    <Route path="/messages/*" element={<Messages users={props.users} messages={props.messages}/>}/>
                     <Route path="/news/*" element={<News/>}/>
                     <Route path="/music/*" element={<Music/>}/>
                     <Route path="/settings/*" element={<Settings/>}/>
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {PostMessageType} from "./components/MainWindow/Profile/MyPosts/Post/Post";
+import {MessageType, UsersType} from "./components/MainWindow/Messages/Messages";
+
+export type AppType = {
+    posts: Array<PostMessageType>
+    users: Array<UsersType>
+    messages: Array<MessageType>
+}
+
+let posts: Array<PostMessageType> = [
+    {message: "Hello", likesCount: 12, id: 1},
+    {message: "Dinosaurus are great", likesCount: 17, id: 2}
+]
+let users: Array<UsersType> = [
+    {name: "Oleg", id: 1},
+    {name: "Diana", id: 2},
+    {name: "Fat cat", id: 2},
+    {name: "Dimka", id: 4},
+    {name: "Homka", id: 5},
+    {name: "Belka", id: 6},
+]
+let messages: Array<MessageType> = [
+    {message: "Hi", id: 1},
+    {message: "Ho", id: 2},
+    {message: "He", id: 3},
+    {message: "Hu", id: 4},
+    {message: "Hio", id: 5},
+    {message: "Hia", id: 6},
+]
 
 ReactDOM.render(
     <BrowserRouter>
-    <App />
+    <App posts={posts} users={users} messages={messages}/>
     </BrowserRouter>,
   document.getElementById('root')
 );
