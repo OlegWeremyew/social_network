d3c030cea8e7530f3708543f0a5863a8f5e22c30
olegweremyew <olegweremey1994@mail.ru>
2021-11-30 20:53:38 +0300

    small change

--- src/components/MainWindow/Profile/MyPosts/MyPosts.tsx
+++ src/components/MainWindow/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {RefObject} from 'react';
+import React, {RefObject, useRef} from 'react';
 import c from "./MyPosts.module.css"
 import {ProfilePageType} from "../../../../redux/state";
 import Post from "./Post/Post";
@@ -13,12 +13,12 @@ const MyPosts = (props: MyPostMessageType) => {
 
     let post = props.state.posts.map(p => <Post message={p.message} likesCount={p.likesCount} id={p.id}/>)
 
-    let newPostElement: RefObject<HTMLTextAreaElement> = React.createRef()
+    let newPostElement = useRef<HTMLTextAreaElement>(null);
 
     let addPost = () => {
         let text = newPostElement.current?.value
         if (text) props.addPost(text)
-        /*newPostElement.current?.value = ""*/
+        if (newPostElement.current) newPostElement.current.value = ''
     }
 
     return (
