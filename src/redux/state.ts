c324f82c40a3d4f4253bb09b09c5be014747e904
olegweremyew <olegweremey1994@mail.ru>
2021-12-09 23:26:05 +0300

    Completed updateNewMessageText

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -39,12 +39,28 @@ let state: RootStateType = {
     },
     messagesPage: {
         users: [
-            {name: "Oleg", id: 1, img: "https://avatars.mds.yandex.net/get-zen_doc/3986249/pub_607ecd1d13c71c1ca8a34192_607ee065330a4f14d551104b/scale_1200"},
-            {name: "Diana", id: 2, img: "https://avatars.mds.yandex.net/i?id=64b380ea3aa0d80cad94cb1a24a3b3f5-4233014-images-thumbs&ref=rim&n=33&w=150&h=150"},
+            {
+                name: "Oleg",
+                id: 1,
+                img: "https://avatars.mds.yandex.net/get-zen_doc/3986249/pub_607ecd1d13c71c1ca8a34192_607ee065330a4f14d551104b/scale_1200"
+            },
+            {
+                name: "Diana",
+                id: 2,
+                img: "https://avatars.mds.yandex.net/i?id=64b380ea3aa0d80cad94cb1a24a3b3f5-4233014-images-thumbs&ref=rim&n=33&w=150&h=150"
+            },
             {name: "Fat cat", id: 3, img: "https://data.whicdn.com/images/310252363/original.jpg"},
             {name: "Dimka", id: 4, img: "https://i.pinimg.com/originals/b7/44/46/b744464dd3d970ad96745be8de69d755.jpg"},
-            {name: "Ella", id: 5, img: "https://st.depositphotos.com/1023162/5099/i/950/depositphotos_50991807-stock-photo-sexy-woman-in-fashion-sunglasses.jpg"},
-            {name: "Makar", id: 6, img: "https://avatars.mds.yandex.net/i?id=55d09aa1629f72a149098b2c3127e039-5222024-images-thumbs&ref=rim&n=33&w=150&h=150"},
+            {
+                name: "Ella",
+                id: 5,
+                img: "https://st.depositphotos.com/1023162/5099/i/950/depositphotos_50991807-stock-photo-sexy-woman-in-fashion-sunglasses.jpg"
+            },
+            {
+                name: "Makar",
+                id: 6,
+                img: "https://avatars.mds.yandex.net/i?id=55d09aa1629f72a149098b2c3127e039-5222024-images-thumbs&ref=rim&n=33&w=150&h=150"
+            },
         ],
         messages: [
             {message: "Hi", id: 1},
@@ -58,6 +74,8 @@ let state: RootStateType = {
     }
 }
 
+//let window.state = state
+
 export let addPost = (postMessage: string) => {
     let newPost = {
         id: 5,
@@ -65,7 +83,8 @@ export let addPost = (postMessage: string) => {
         likesCount: 12
     }
 
-    state. profilePage.posts.push(newPost)
+    state.profilePage.posts.push(newPost)
+    state.profilePage.newPostText = ""
     rerenderEntireTree(state)
 }
 
@@ -81,6 +100,7 @@ export let addMessage = (AddNewMessage: string) => {
     }
 
     state.messagesPage.messages.push(newMessage)
+    state.messagesPage.newMessageText = ""
     rerenderEntireTree(state)
 }
 
